d65e8239abc29de5a1911094c56af8f4ff14845a
hossainiqbal <hossainiqballf@gmail.com>
2020-11-26 11:15:14 -0500

    adding loger class

--- src/test/java/com/utilities/Utility.java
+++ src/test/java/com/utilities/Utility.java
@@ -1,5 +1,33 @@
 package com.utilities;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Utility {
 
+	public static Logger getLog(Class clazz) {
+		Logger log = Logger.getLogger(clazz);
+		PropertyConfigurator.configure("src/test/resources/File/log4j.properties");
+		return log;
+
+	}
+
+	public static Properties readPropFile() {
+		Properties p = new Properties();
+		try {
+			FileInputStream f = new FileInputStream("src/test/resources/Driver/chromedriver.exe");
+			p.load(f);
+
+		} catch (Exception e) {
+
+			System.out.println(e.getMessage());
+
+		}
+		return p;
+	}
+
+	
 }
