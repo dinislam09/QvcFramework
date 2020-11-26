393ab480a1cce1ecbabc9eb3824f2fbfb448f884
shahidul1988 <shahidultex@gmail.com>
2020-11-27 00:18:59 +0600

    i uptodate browser class

--- src/main/java/com/base/Browser.java
+++ src/main/java/com/base/Browser.java
@@ -7,45 +7,63 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
-	static WebDriver dr=null;
-	static String basePath=System.getProperty("user.dir");
-	 public static void main (String []args){
-		  Browser.openBrowser("chrome");
-		 }
+	static WebDriver dr = null;
+	static String basePath = System.getProperty("user.dir");
+
+	public static void main(String[] args) {
+		Browser.openBrowser("chrome");
+	}
+
 	public static WebDriver openBrowser(String browser) {
 
-		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+		if (System.getProperty("os.name").contains("windows")) {
 			getBrowserForWindows(browser);
-		}else{
+		} else {
 			getBrowserForMac(browser);
 		}
+		dr.get("https://www.qvc.com/");
 		dr.manage().window().maximize();
-		dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		dr.manage().deleteAllCookies();
-		
+		dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		dr.close();
 		return dr;
 	}
+
 	public static void getBrowserForWindows(String browser) {
-		if(browser.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", basePath+"src\\test\\resources\\Driver\\geckodriver.exe");
-			 dr=new FirefoxDriver();
-		}else if(browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", basePath+"src\\test\\resources\\Driver\\chromedriver.exe");
-			 dr= new ChromeDriver();
-		}else {
-			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
+		if (browser.equals("firefox")) {
+			System.setProperty(
+					"webdriver.gecko.driver",
+					basePath
+					+ "//src/test//resources//Driver//geckodriver.exe");
+			dr = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			System.setProperty(
+					"webdriver.chrome.driver",
+					basePath
+					+ "//src//test//resources//Driver//chromedriver.exe");
+			dr = new ChromeDriver();
+		} else {
+			System.out.println("You have entered " + browser
+					+ " that does not exist. Please either chrome or firefox");
 		}
+	}
+
+	public static void getBrowserForMac(String browser) {
+		if (browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", basePath
+					+ "//src/test//resources//Driver//geckodriver.exe");
+			dr = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", basePath
+					+ "//src//test//resources//Driver//chromedriver.exe");
+			dr = new ChromeDriver();
+		} else {
+			System.out.println("You have entered " + browser
+					+ " that does not exist. Please either chrome or firefox");
+		}
+	
 	}
 	
-	public static void getBrowserForMac(String browser) {
-		if(browser.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", basePath+"src\\test\\resources\\Driver\\geckodriver.exe");
-			 dr=new FirefoxDriver();
-		}else if(browser.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", basePath+"src\\test\\resources\\Driver\\chromedriver.exe");
-			 dr= new ChromeDriver();
-		}else {
-			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
-		}
-	}
+	
 }
+	
