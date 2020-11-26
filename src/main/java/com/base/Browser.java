a50b31179fcee7ed48309ddfe92ab00849978a7c
Poly Chowdhury101 <abiachowdhury1@gmail.com>
2020-11-26 19:56:42 +0300

    I am adding browser file for new framework

--- src/main/java/com/base/Browser.java
+++ src/main/java/com/base/Browser.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Browser {
 	static WebDriver dr=null;
 	static String basePath=System.getProperty("user.dir");
+	 public static void main (String []args){
+		  Browser.openBrowser("chrome");
+		 }
 	public static WebDriver openBrowser(String browser) {
 
 		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
@@ -16,14 +19,18 @@ public class Browser {
 		}else{
 			getBrowserForMac(browser);
 		}
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		dr.manage().deleteAllCookies();
+		
 		return dr;
 	}
 	public static void getBrowserForWindows(String browser) {
 		if(browser.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", basePath+"\\src\\test\\resources\\drivers\\windows\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", basePath+"src\\test\\resources\\Driver\\geckodriver.exe");
 			 dr=new FirefoxDriver();
 		}else if(browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", basePath+"\\src\\test\\resources\\drivers\\windows\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", basePath+"src\\test\\resources\\Driver\\chromedriver.exe");
 			 dr= new ChromeDriver();
 		}else {
 			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
@@ -32,10 +39,10 @@ public class Browser {
 	
 	public static void getBrowserForMac(String browser) {
 		if(browser.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", basePath+"\\src\\test\\resources\\drivers\\mac\\geckodriver");
+			System.setProperty("webdriver.gecko.driver", basePath+"src\\test\\resources\\Driver\\geckodriver.exe");
 			 dr=new FirefoxDriver();
 		}else if(browser.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", basePath+"\\src\\test\\resources\\drivers\\mac\\chromedriver");
+			System.setProperty("webdriver.chrome.driver", basePath+"src\\test\\resources\\Driver\\chromedriver.exe");
 			 dr= new ChromeDriver();
 		}else {
 			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
